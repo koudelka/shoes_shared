5c283573aa4c2d9a5af5c1c1d6ca32940d1ebc6c
Michael Shapiro <koudelka@ryoukai.org>
2009-03-06 22:45:19 -0500

    Grumble.

--- shoes_shared.rb
+++ shoes_shared.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__)+'lib/point'
-require File.dirname(__FILE__)+'lib/area'
+require File.dirname(__FILE__)+'/lib/point'
+require File.dirname(__FILE__)+'/lib/area'
 
-require File.dirname(__FILE__)+'ext/lib/shoes/shape'
+require File.dirname(__FILE__)+'/ext/lib/shoes/shape'
